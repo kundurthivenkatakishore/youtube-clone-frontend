bec769987063ae361adc55c3475bb1164c1e6b08
kundurthivenkatakishore <kundurthivenkatakishore@gmail.com>
2022-05-26 15:59:28 +0530

    Updates

---
+++ precache-manifest.617e7ff097efe8e7ff6b9cc1309be0b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37051a5462167143959566eeffc9cf1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9bcbdb2469ab63467c2",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "2344a00423f393d8c60c",
+    "url": "/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "f9bcbdb2469ab63467c2",
+    "url": "/static/js/2.fe490704.chunk.js"
+  },
+  {
+    "revision": "2344a00423f393d8c60c",
+    "url": "/static/js/main.7f2965e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ee8d6a1ba2918284c7bde098919a8ce",
+    "url": "/static/media/youtubelogo.5ee8d6a1.png"
+  }
+]);
